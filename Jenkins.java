731a387e8cf3ec1e2aadb497d3b95d7e7c571816
Vishal Ahir <54415582+ahirvishal@users.noreply.github.com>
2019-11-21 17:33:39 +0530

    dsd

--- Jenkins.java
+++ Jenkins.java
@@ -1,8 +1,12 @@
 public class Jenkins
 {
+	public static void add()
+	{
+		System.out.println("from Function");
+	}
 	public static void main(String[] args)
 	{
 			System.out.println("hjasjhslkda");
+			add();
 	}
 }
- 
