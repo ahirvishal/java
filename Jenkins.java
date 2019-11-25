ba16e5a9846682dbc8c3f45cbb328d780a5de646
Vishal Ahir <54415582+ahirvishal@users.noreply.github.com>
2019-11-25 14:43:11 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -1,12 +1,7 @@
 public class Jenkins
 {
-	public static void add()
-	{
-		System.out.println("from Function");
-	}
 	public static void main(String[] args)
 	{
 			System.out.println("hjasjhslkda");
-			add();
 	}
 }
